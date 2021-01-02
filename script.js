a37e1a756a1846e8e8ab92ae69016f02c130f902
Alwin Villero <70043892+winal-28@users.noreply.github.com>
2021-01-02 04:27:36 -0800

    The script for the clock

---
+++ script.js
@@ -0,0 +1,25 @@
+setInterval(setClock, 1000);
+
+//hands 
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+
+//setup of the clock 
+
+function setClock(){
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+};
+
+//rotate the hands 
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360);
+};
